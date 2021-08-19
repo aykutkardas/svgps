3caed62545e9200827d91e14c51c6dee7da5cd4a
Aykut Kardaş <aykutkrds@gmail.com>
2021-08-19 19:54:12 +0300

    Update getPaths return data type to Array

--- src/getPaths.js
+++ src/getPaths.js
@@ -1,6 +1,6 @@
 function getPaths(svg) {
   let paths = svg.match(/ d="[^"]*"/gim);
-  if (paths) paths = paths.map((i) => i.replace(/\"| d=/gim, "")).join(" ");
+  if (paths) paths = paths.map((i) => i.replace(/\"| d=/gim, ""));
   return paths;
 }
 
