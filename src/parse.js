2541d951f99702c2a7683b2c99d6d3cbbb09961a
Aykut Kardaş <aykutkrds@gmail.com>
2021-08-20 00:03:46 +0300

    Add `polygon` support

--- src/parse.js
+++ src/parse.js
@@ -1,15 +1,33 @@
 const { parse } = require("muninn");
 
 function parseSVG(svg) {
-  return parse(svg, {
+  const data = parse(svg, {
     schema: {
-      paths: "path@d | array",
-      fills: "path@fill | array",
-      width: "svg@width | number",
-      height: "svg@height | number",
-      viewBox: "svg@viewBox",
+      points: {
+        selector: "polygon @ points | array",
+        custom: function (val) {
+          if (val) {
+            return val.startsWith("M") ? val : "M" + val;
+          }
+
+          return val;
+        },
+      },
+      paths: "path @ d | array",
+      fills: "polygon, path @ fill | array",
+      width: "svg @ width | number",
+      height: "svg @ height | number",
+      viewBox: "svg @ viewBox",
     },
   });
+
+  data.points.forEach((point) => {
+    data.paths.unshift(point);
+  });
+
+  delete data.points;
+
+  return data;
 }
 
 module.exports = parseSVG;
