c728a012a95df685e7cfa039ae660c7196e1bb4f
Aykut Kardaş <aykutkrds@gmail.com>
2022-07-22 18:34:24 +0300

    Remove getFormattedName & cli

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-const getFormattedName = require("./getFormattedName");
 const parse = require("./parse");
-const convertToIcomoonFormat = require("./convertToIcomoonFormat");
 
-module.exports = {
-  convertToIcomoonFormat,
-  getFormattedName,
-  parse,
-};
+module.exports = { parse };
